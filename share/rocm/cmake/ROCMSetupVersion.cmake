8639c1032a65070fe9824046e47a592f40941077
Paul <pfultz2@yahoo.com>
2017-05-16 18:13:39 -0500

    Move get version into setup version module

--- share/rocm/cmake/ROCMSetupVersion.cmake
+++ share/rocm/cmake/ROCMSetupVersion.cmake
@@ -4,8 +4,38 @@ macro(rocm_set_parent VAR)
     set(${VAR} ${ARGN})
 endmacro()
 
-function(rocm_setup_version)
+function(rocm_get_version OUTPUT_VERSION)
     set(options)
+    set(oneValueArgs VERSION DIRECTORY)
+    set(multiValueArgs)
+
+    cmake_parse_arguments(PARSE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    set(_version ${PARSE_VERSION})
+
+    set(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+    if(PARSE_DIRECTORY)
+        set(DIRECTORY ${PARSE_DIRECTORY})
+    endif()
+
+    find_program(GIT NAMES git)
+
+    if(GIT)
+        execute_process(COMMAND git describe --dirty --long --match [0-9]*
+                        WORKING_DIRECTORY ${DIRECTORY}
+                        OUTPUT_VARIABLE GIT_TAG_VERSION
+                        OUTPUT_STRIP_TRAILING_WHITESPACE
+                        RESULT_VARIABLE RESULT)
+        if(${RESULT} EQUAL 0)
+            set(_version ${GIT_TAG_VERSION})
+        endif()
+    endif()
+    rocm_set_parent(${OUTPUT_VERSION} ${_version})
+
+endfunction()
+
+function(rocm_setup_version)
+    set(options NO_GIT_TAG_VERSION)
     set(oneValueArgs VERSION)
     set(multiValueArgs)
 
@@ -18,7 +48,12 @@ function(rocm_setup_version)
     endif()
 
     if(PARSE_VERSION)
-        rocm_set_parent(PROJECT_VERSION ${PARSE_VERSION})
+        if(NO_GIT_TAG_VERSION)
+            set(PACKAGE_VERSION ${PARSE_VERSION})
+        else()
+            rocm_get_version(PACKAGE_VERSION VERSION ${PARSE_VERSION})
+        endif()
+        rocm_set_parent(PROJECT_VERSION ${PACKAGE_VERSION})
         rocm_set_parent(${PROJECT_NAME}_VERSION ${PROJECT_VERSION})
         string(REGEX REPLACE "^([0-9]+)\\.[0-9]+\\.[0-9]+.*" "\\1" _version_MAJOR "${PROJECT_VERSION}")
         string(REGEX REPLACE "^[0-9]+\\.([0-9]+)\\.[0-9]+.*" "\\1" _version_MINOR "${PROJECT_VERSION}")
