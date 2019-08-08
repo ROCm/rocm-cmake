07a27b2863703804fa36fde8abc3735364db9180
radhaksri <47536883+radhaksri@users.noreply.github.com>
2019-08-08 11:03:01 -0700

    Fixing test to check ROCM_BUILD_ID as part of setup

--- test/pass/version-build-id.cmake
+++ test/pass/version-build-id.cmake
@@ -17,7 +17,7 @@ write_version_cmake(${TMP_DIR}/repo 1.0 "
     test_expect_eq(\${PROJECT_VERSION_MAJOR} 1)
     test_expect_eq(\${PROJECT_VERSION_MINOR} 0)
     test_expect_eq(\${PROJECT_VERSION_PATCH} 0)
-    test_expect_eq(\${PROJECT_VERSION_TWEAK} ${REVS}-job-7-${GIT_TAG})
+    test_expect_eq(\${PROJECT_VERSION_TWEAK} ${REVS}-rocm-rel-x.y-72-${GIT_TAG})
     test_expect_eq(\${PROJECT_VERSION} \${PROJECT_VERSION_MAJOR}.\${PROJECT_VERSION_MINOR}.\${PROJECT_VERSION_PATCH}.\${PROJECT_VERSION_TWEAK})
 ")
 install_dir(${TMP_DIR}/repo)
