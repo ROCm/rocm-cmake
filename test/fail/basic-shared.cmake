7f542ec79f336c3936436b047df07b34e8c57004
Paul <pfultz2@yahoo.com>
2020-03-30 16:22:50 -0500

    Add tests to check if the target exists or not

--- test/fail/basic-shared.cmake
+++ test/fail/basic-shared.cmake
@@ -2,7 +2,7 @@
 # Copyright (C) 2020 Advanced Micro Devices, Inc.
 ################################################################################
 
-install_dir(${TEST_DIR}/libsimple CMAKE_ARGS -DBUILD_SHARED_LIBS=Off)
+install_dir(${TEST_DIR}/libsimple CMAKE_ARGS -DBUILD_SHARED_LIBS=On)
 test_check_package(NAME simple HEADER simple.h TARGET simple)
-install_dir(${TEST_DIR}/libbasic CMAKE_ARGS -DBUILD_SHARED_LIBS=Off)
+install_dir(${TEST_DIR}/libbasic CMAKE_ARGS -DBUILD_SHARED_LIBS=On)
 test_check_package(NAME basic HEADER basic.h TARGET basic CHECK_TARGET simple)
