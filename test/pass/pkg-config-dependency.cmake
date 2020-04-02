8b0fb6c598a05cd9286fd0fb40ec8cf3cd68eb0f
Paul <pfultz2@yahoo.com>
2020-04-02 15:37:18 -0500

    Add support for pkg-config in dependency list

---
+++ test/pass/pkg-config-dependency.cmake
@@ -0,0 +1,6 @@
+################################################################################
+# Copyright (C) 2020 Advanced Micro Devices, Inc.
+################################################################################
+
+install_dir(${TEST_DIR}/libsimplez CMAKE_ARGS -DBUILD_SHARED_LIBS=On)
+test_check_package(NAME simplez HEADER simplez.h TARGET simplez)
---
+++ test/pass/toolchain-var.cmake
@@ -0,0 +1,5 @@
+################################################################################
+# Copyright (C) 2020 Advanced Micro Devices, Inc.
+################################################################################
+
+configure_dir(${TEST_DIR}/toolchain-var CMAKE_ARGS -DCHANGE_TOOLCHAIN=Off)
