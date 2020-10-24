54220a591aabdcb1a1f4fab922ced3c0e223013c
Paul <pfultz2@yahoo.com>
2020-10-23 19:16:55 -0500

    Only go back 10 revs

--- test/pass/version-parent.cmake
+++ test/pass/version-parent.cmake
@@ -20,7 +20,7 @@ execute_process(
     OUTPUT_VARIABLE REVS
     RESULT_VARIABLE RESULT)
 separate_arguments(REVS UNIX_COMMAND "${REVS}")
-list(GET REVS 50 PARENT)
+list(GET REVS 10 PARENT)
 string(STRIP "${REVS}" REVS)
 message("PARENT: ${PARENT}")
 write_version_cmake(
@@ -30,7 +30,7 @@ write_version_cmake(
     test_expect_eq(\${PROJECT_VERSION_MAJOR} 1)
     test_expect_eq(\${PROJECT_VERSION_MINOR} 0)
     test_expect_eq(\${PROJECT_VERSION_PATCH} 0)
-    test_expect_eq(\${PROJECT_VERSION_TWEAK} 50-${GIT_TAG})
+    test_expect_eq(\${PROJECT_VERSION_TWEAK} 10-${GIT_TAG})
     test_expect_eq(\${PROJECT_VERSION}
         \${PROJECT_VERSION_MAJOR}.\${PROJECT_VERSION_MINOR}.\${PROJECT_VERSION_PATCH}.\${PROJECT_VERSION_TWEAK})
 ")
