ae89d7681533c08e8c56ae8403e11080f1b1ce4f
Paul <pfultz2@yahoo.com>
2020-11-24 17:47:43 -0600

    FIx typos

--- test/pass/analyze.cmake
+++ test/pass/analyze.cmake
@@ -2,4 +2,4 @@
 # Copyright (C) 2017 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-install_dir(${TEST_DIR}/analyze TARGETS analyze)
+install_dir(${TEST_DIR}/analyze TARGETS analyze analyze)
