cf374be2e86ab08981a5842c1000c29734f5dbff
Paul <pfultz2@yahoo.com>
2020-11-25 22:11:39 -0600

    Update test

--- test/fail/toolchain-var.cmake
+++ test/fail/toolchain-var.cmake
@@ -2,4 +2,8 @@
 # Copyright (C) 2019 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
+if(UNIX)
 configure_dir(${TEST_DIR}/toolchain-var CMAKE_ARGS -DCHANGE_TOOLCHAIN=On)
+else()
+message(FATAL_ERROR "Skipped on windows")
+endif()
