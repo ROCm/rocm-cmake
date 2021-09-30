5207082b15910cf0157723b790d4a3728d3d1b31
Liam Wrubleski <Liam.Wrubleski@amd.com>
2021-09-30 17:28:03 -0600

    Adding tests

---
+++ test/pass/rocm-find-program-version.cmake
@@ -0,0 +1,34 @@
+use_rocm_cmake()
+include(ROCMUtilities)
+
+rocm_find_program_version(
+    cmake
+    OUTPUT_VARIABLE test_version
+)
+
+test_expect_eq("${test_version}" "${CMAKE_VERSION}")
+test_expect_eq("${test_version_OK}" TRUE)
+
+rocm_find_program_version(
+    cmake
+    OUTPUT_VARIABLE test_version
+    GREATER_THAN "${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.0"
+)
+
+test_expect_eq("${test_version_OK}" TRUE)
+
+rocm_find_program_version(
+    cmake
+    OUTPUT_VARIABLE test_version
+    LESS "${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}.0"
+)
+
+test_expect_eq("${test_version_OK}" FALSE)
+
+rocm_find_program_version(
+    foobar
+    OUTPUT_VARIABLE test_version
+)
+
+test_expect_eq("${test_version}" "0.0.0")
+test_expect_eq("${test_version_OK}" FALSE)
