b2aa6dca9beee74c3add053ef3ea8d7c4257211e
Torre Zuk <42548444+TorreZuk@users.noreply.github.com>
2021-12-14 16:32:52 -0700

    clarify warning/error and adds env control (#62)

--- share/rocm/cmake/ROCMChecks.cmake
+++ share/rocm/cmake/ROCMChecks.cmake
@@ -1,5 +1,5 @@
 # ######################################################################################################################
-# Copyright (C) 2019 Advanced Micro Devices, Inc.
+# Copyright (C) 2019-2021 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
 set(ROCM_WARN_TOOLCHAIN_VAR
@@ -8,14 +8,22 @@ set(ROCM_WARN_TOOLCHAIN_VAR
 set(ROCM_ERROR_TOOLCHAIN_VAR
     OFF
     CACHE BOOL "")
+# environment variable control of options. Note prefix is ROCMCHECKS
+if(DEFINED ENV{ROCMCHECKS_WARN_TOOLCHAIN_VAR})
+    set(ROCM_WARN_TOOLCHAIN_VAR $ENV{ROCMCHECKS_WARN_TOOLCHAIN_VAR})
+endif()
+if(DEFINED ENV{ROCMCHECKS_ERROR_TOOLCHAIN_VAR})
+    set(ROCM_ERROR_TOOLCHAIN_VAR $ENV{ROCMCHECKS_ERROR_TOOLCHAIN_VAR})
+endif()
 
 function(rocm_check_toolchain_var var access value list_file)
     set(message_type STATUS)
-    if(ROCM_WARN_TOOLCHAIN_VAR)
-        set(message_type WARNING)
-    endif()
     if(ROCM_ERROR_TOOLCHAIN_VAR)
         set(message_type SEND_ERROR)
+        set(message_title " ROCMChecks ERROR ")
+    elseif(ROCM_WARN_TOOLCHAIN_VAR)
+        set(message_type WARNING)
+        set(message_title "ROCMChecks WARNING")
     endif()
     if(access STREQUAL "MODIFIED_ACCESS")
         set(cmake_module Off)
@@ -27,22 +35,22 @@ function(rocm_check_toolchain_var var access value list_file)
             set(cmake_module On)
         endif()
         if(NOT cmake_module)
-            message(
-                "
+            message( "
 *******************************************************************************
-*----------------------------------- ERROR -----------------------------------*
-* The variable '${var}' should only be set by the cmake toolchain,
-* either by calling 'cmake -D${var}=\"${value}\"' or
-* set in a toolchain file and added with
-* 'cmake -DCMAKE_TOOLCHAIN_FILE=<toolchain-file>'.
-*-----------------------------------------------------------------------------*
+*------------------------------- ${message_title} --------------------------*
+  Options and properties should be set on a cmake target where possible. The
+  variable '${var}' may be set by the cmake toolchain, either by
+  calling 'cmake -D${var}=\"${value}\"'
+  or set in a toolchain file and added with
+  'cmake -DCMAKE_TOOLCHAIN_FILE=<toolchain-file>'. ROCMChecks now calling:")
+            message(${message_type} "'${var}' is set at ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt:<line#> shown below:")
+            message( "*-----------------------------------------------------------------------------*
 *******************************************************************************
 ")
-            message(${message_type} "The toolchain variable '${var}' is modified in the CMakeLists.txt.")
         endif()
     endif()
 endfunction()
-if(UNIX)
+if(UNIX AND (ROCM_WARN_TOOLCHAIN_VAR OR ROCM_ERROR_TOOLCHAIN_VAR))
     foreach(LANG C CXX Fortran)
         variable_watch(CMAKE_${LANG}_COMPILER rocm_check_toolchain_var)
         variable_watch(CMAKE_${LANG}_FLAGS rocm_check_toolchain_var)
