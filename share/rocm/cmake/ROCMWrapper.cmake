05e0ef472657c6ffb6882c910e8ae7ee43e4ed93
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-02-10 15:12:49 -0700

    fix paths

--- share/rocm/cmake/ROCMWrapper.cmake
+++ share/rocm/cmake/ROCMWrapper.cmake
@@ -3,7 +3,8 @@
 # ######################################################################################################################
 
 function(rocm_wrap_header_dir)
-    cmake_parse_arguments(PARSE "" "DIRECTORY;GUARD;LOCATION" "PATTERNS" ${argv})
+    message(STATUS "rocm_wrap_header_dir args: ${ARGN}")
+    cmake_parse_arguments(PARSE "" "DIRECTORY;GUARD;LOCATION" "PATTERNS" ${ARGN})
     file (GLOB_RECURSE include_files RELATIVE "${PARSE_DIRECTORY}" ${PARSE_PATTERNS})
     foreach (include_file ${include_files})
         if (NOT "${include_file}" MATCHES "^../")
@@ -17,9 +18,9 @@ function(rocm_wrap_header_dir)
 endfunction()
 
 function(rocm_wrap_header_file)
-    cmake_parse_arguments(PARSE "" "INCLUDE_FILE;GUARD;LOCATION;INSTALL_LOCATION" "" ${argv})
+    cmake_parse_arguments(PARSE "" "INCLUDE_FILE;GUARD;LOCATION;INSTALL_LOCATION" "" ${ARGN})
     if(NOT PARSE_INSTALL_LOCATION)
-        set(PARSE_INSTALL_LOCATION "include/${CMAKE_PROJECT_NAME}")
+        set(PARSE_INSTALL_LOCATION "${PROJECT_BINARY_DIR}/include/${CMAKE_PROJECT_NAME}")
     endif()
     get_filename_component(file_name ${PARSE_INCLUDE_FILE} NAME)
     get_filename_component(file_path ${PARSE_INCLUDE_FILE} DIRECTORY)
@@ -35,12 +36,13 @@ function(rocm_wrap_header_file)
     string(TOUPPER ${file_name} file_name)
     string(REGEX REPLACE "[-.]" "_" file_name "${file_name}")
     set(include_guard "ROCM_${PARSE_GUARD}${guard}_${file_name}")
+    set(wrapper_location "${PROJECT_BINARY_DIR}/${PARSE_LOCATION}/${file_path}")
     file(RELATIVE_PATH include_statements 
-        "${PROJECT_BINARY_DIR}/${PARSE_LOCATION}"
+        "${wrapper_location}"
         "${PARSE_INSTALL_LOCATION}/${include_file}"
     )
     configure_file(
         "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/header_template.h.in"
-        "${PARSE_INSTALL_LOCATION}/${PARSE_LOCATION}"
+        "${PROJECT_BINARY_DIR}/${PARSE_LOCATION}/${PARSE_INCLUDE_FILE}"
     )
 endfunction()
