ad9209704aad33f5fb60dbf2e8523af9d2ff34e3
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-02-18 10:20:53 -0700

    Allow rocm_wrap_header_file to wrap multiple

--- share/rocm/cmake/ROCMWrapper.cmake
+++ share/rocm/cmake/ROCMWrapper.cmake
@@ -30,41 +30,43 @@ function(rocm_wrap_header_dir DIRECTORY)
 endfunction()
 
 function(rocm_wrap_header_file)
-    cmake_parse_arguments(PARSE "" "HEADER_LOCATION" "GUARDS;WRAPPER_LOCATIONS;OUTPUT_LOCATIONS" ${ARGN})
+    cmake_parse_arguments(PARSE "" "HEADER_LOCATION" "GUARDS;WRAPPER_LOCATIONS;OUTPUT_LOCATIONS;HEADERS" ${ARGN})
+    set(PARSE_HEADERS ${PARSE_HEADERS} ${PARSE_UNPARSED_ARGUMENTS})
     if(NOT PARSE_HEADER_LOCATION)
         set(PARSE_HEADER_LOCATION "include/${CMAKE_PROJECT_NAME}")
     endif()
-    get_filename_component(header_location "${PARSE_HEADER_LOCATION}/${INCLUDE_FILE}"
-        ABSOLUTE BASE_DIR "${CMAKE_BINARY_DIR}")
-    get_filename_component(file_name ${INCLUDE_FILE} NAME)
-    get_filename_component(file_path ${INCLUDE_FILE} DIRECTORY)
-    string(REPLACE "/" ";" path_dirs "${file_path}")
+    foreach(INCLUDE_FILE in PARSE_HEADERS)
+        get_filename_component(header_location "${PARSE_HEADER_LOCATION}/${INCLUDE_FILE}"
+            ABSOLUTE BASE_DIR "${CMAKE_BINARY_DIR}")
+        get_filename_component(file_name ${INCLUDE_FILE} NAME)
+        get_filename_component(file_path ${INCLUDE_FILE} DIRECTORY)
+        string(REPLACE "/" ";" path_dirs "${file_path}")
 
-    set(guard_common "")
-    foreach(subdir IN LISTS path_dirs)
-        if(NOT (subdir STREQUAL '' OR subdir STREQUAL '.'))
-            string(TOUPPER ${subdir} subdir_uc)
-            set(guard_common "${guard_common}_${subdir_uc}")
-        endif()
+        set(guard_common "")
+        foreach(subdir IN LISTS path_dirs)
+            if(NOT (subdir STREQUAL '' OR subdir STREQUAL '.'))
+                string(TOUPPER ${subdir} subdir_uc)
+                set(guard_common "${guard_common}_${subdir_uc}")
+            endif()
+        endforeach()
+        string(TOUPPER ${file_name} file_name)
+        string(REGEX REPLACE "[-.]" "_" file_name "${file_name}")
+        set(guard_common "${guard_common}_${file_name}")
+
+        # do-while
+        set(first_time true)
+        while(first_time OR PARSE_GUARDS OR PARSE_WRAPPER_LOCATIONS OR PARSE_OUTPUT_LOCATIONS)
+            rocm_wrap_header_get_info(ITEM PARSE_GUARDS PARSE_WRAPPER_LOCATIONS PARSE_OUTPUT_LOCATIONS)
+            set(include_guard "ROCM_${ITEM_GUARD}${guard_common}")
+            set(wrapper_location "${ITEM_WRAPPER_LOCATION}/${file_path}")
+            file(RELATIVE_PATH file_rel_path "${wrapper_location}" "${header_location}")
+            configure_file(
+                "${_ROCM_WRAPPER_DIR}/header_template.h.in"
+                "${ITEM_OUTPUT_LOCATION}/${INCLUDE_FILE}"
+            )
+            set(first_time false)
+        endwhile()
     endforeach()
-    string(TOUPPER ${file_name} file_name)
-    string(REGEX REPLACE "[-.]" "_" file_name "${file_name}")
-    set(guard_common "${guard_common}_${file_name}")
-
-    # do-while
-    set(first_time true)
-    while(first_time OR PARSE_GUARDS OR PARSE_WRAPPER_LOCATIONS OR PARSE_OUTPUT_LOCATIONS)
-        rocm_wrap_header_get_info(ITEM PARSE_GUARDS PARSE_WRAPPER_LOCATIONS PARSE_OUTPUT_LOCATIONS)
-        set(include_guard "ROCM_${ITEM_GUARD}${guard_common}")
-        set(wrapper_location "${ITEM_WRAPPER_LOCATION}/${file_path}")
-        file(RELATIVE_PATH file_rel_path "${wrapper_location}" "${header_location}")
-        configure_file(
-            "${_ROCM_WRAPPER_DIR}/header_template.h.in"
-            "${ITEM_OUTPUT_LOCATION}/${INCLUDE_FILE}"
-        )
-        set(first_time false)
-    endwhile()
-
 endfunction()
 
 # internal
