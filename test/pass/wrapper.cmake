f0af201fd7e21ec1fac56064fe8504c551d8737c
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-02-18 13:50:49 -0700

    Add wrapper test

---
+++ test/pass/wrapper.cmake
@@ -0,0 +1,14 @@
+# ######################################################################################################################
+# Copyright (C) 2020 Advanced Micro Devices, Inc.
+# ######################################################################################################################
+
+install_dir(${TEST_DIR}/libwrapper CMAKE_ARGS -DBUILD_SHARED_LIBS=On -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=On)
+test_check_package(
+    NAME wrapper
+    HEADER wrapper.h
+    TARGET wrapper)
+test_check_package(
+    NAME wrapper
+    HEADER wrapper/wrapper.h
+    TARGET wrapper
+)
