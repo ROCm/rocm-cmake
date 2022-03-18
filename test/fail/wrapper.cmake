b6be0802d1bea74c59a04b13d808281dd3489106
Liam Wrubleski <Liam.Wrubleski@amd.com>
2022-03-18 10:33:38 -0600

    correct formatting

--- test/fail/wrapper.cmake
+++ test/fail/wrapper.cmake
@@ -2,7 +2,8 @@
 # Copyright (C) 2022 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-install_dir(${TEST_DIR}/libwrapper CMAKE_ARGS -DBUILD_SHARED_LIBS=On -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=On -DERR_ON_WARN=ON)
+install_dir(${TEST_DIR}/libwrapper
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=On -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=On -DERR_ON_WARN=ON)
 test_check_package(
     NAME test-wrapper
     HEADER wrapper.h
