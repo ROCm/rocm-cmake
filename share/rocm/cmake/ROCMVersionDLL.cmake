a8d12517e7173e1671bc9f03f8d65c8e7f96e150
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2022-11-09 10:09:17 -0700

    Correct rc template file location

--- share/rocm/cmake/ROCMVersionDLL.cmake
+++ share/rocm/cmake/ROCMVersionDLL.cmake
@@ -2,6 +2,8 @@
 # Copyright (C) 2022 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
+set(_ROCMVersionDLL_LIST_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
 function(rocm_version_dll TARGET)
     if(WIN32)
         if(DEFINED ENV{ROCM_DLL_VERSION} AND NOT "$ENV{ROCM_DLL_VERSION}" STREQUAL "")
@@ -9,7 +11,7 @@ function(rocm_version_dll TARGET)
         endif()
         if(ROCM_DLL_VERSION)
             string(REPLACE "." "," ROCM_DLL_VERSION_COMMA "${ROCM_DLL_VERSION}")
-            configure_file(${CMAKE_CURRENT_LIST_DIR}/version.rc.in ${CMAKE_CURRENT_BINARY_DIR}/version_${TARGET}.rc @ONLY)
+            configure_file(${_ROCMVersionDLL_LIST_DIR}/version.rc.in ${CMAKE_CURRENT_BINARY_DIR}/version_${TARGET}.rc @ONLY)
             set_property(TARGET ${TARGET} APPEND PROPERTY SOURCES "${CMAKE_CURRENT_BINARY_DIR}/version_${TARGET}.rc")
         endif()
     endif()
