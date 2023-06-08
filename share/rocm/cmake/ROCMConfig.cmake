6001f532137e7dd290fb74fb5c482636d75ed9db
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-06-08 14:59:09 -0600

    Copyright info

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -1,3 +1,7 @@
+# ######################################################################################################################
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# ######################################################################################################################
+
 message(INFO
     "Use of find_package(ROCM) is deprecated as of <version>, and will be removed in <version>, "
     "please switch to find_package(ROCmCMakeBuildTools)."
--- share/rocm/cmake/ROCMConfigVersion.cmake
+++ share/rocm/cmake/ROCMConfigVersion.cmake
@@ -1,3 +1,7 @@
+# ######################################################################################################################
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# ######################################################################################################################
+
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR}/../../rocmcmakebuildtools/cmake)
 
 include(ROCmCMakeBuildToolsConfigVersion)
