27fe29ceabc30fe36fdfc003dc58ecf818a052f7
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-07-13 13:05:37 -0600

    Update deprecation message and type

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -2,8 +2,7 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-message(INFO
-    "Use of find_package(ROCM) is deprecated as of <version>, and will be removed in <version>, "
-    "please switch to find_package(ROCmCMakeBuildTools)."
+message(DEPRECATION
+    "Use of find_package(ROCM) is deprecated, please switch to find_package(ROCmCMakeBuildTools)."
 )
 find_package(ROCmCMakeBuildTools PATHS .)
