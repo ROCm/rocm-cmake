bb2cbef8be049426957e43f6b2c9d1a6ca35e2fa
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-08-18 10:19:07 -0600

    Change DEPRECATION to NOTICE
    In order to have as smooth a transition as possible, starting this as
    a notice, will make into a deprecation/warning in ~6.2, error sometime
    later, remove in 7.0.

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-message(DEPRECATION
+message(NOTICE
     "Use of find_package(ROCM) is deprecated, please switch to find_package(ROCmCMakeBuildTools)."
 )
 find_package(ROCmCMakeBuildTools HINTS "${CMAKE_CURRENT_LIST_DIR}")
