a19ed4ccec05a2c913eb24fc026551c411f4f519
Paul <pfultz2@yahoo.com>
2023-09-01 12:21:05 -0500

    Add test

---
+++ test/pass/simple-test-package.cmake
@@ -0,0 +1,11 @@
+# ######################################################################################################################
+# Copyright (C) 2023 Advanced Micro Devices, Inc.
+# ######################################################################################################################
+
+configure_dir(${TEST_DIR}/libsimpletest TARGETS package BUILD_DIR_VAR BUILD_DIR)
+file(GLOB TEST_PACKAGE ${BUILD_DIR}/*-tests.tar.gz)
+list(LENGTH TEST_PACKAGE NTEST_PACKAGE)
+test_expect_eq(${NTEST_PACKAGE} 1)
+test_exec(COMMAND ${CMAKE_COMMAND} -E tar t ${TEST_PACKAGE})
+test_exec(COMMAND ${CMAKE_COMMAND} -E tar x ${TEST_PACKAGE} WORKING_DIRECTORY /)
+test_expect_file(${PREFIX}/share/test/simple/CTestTestfile.cmake)
