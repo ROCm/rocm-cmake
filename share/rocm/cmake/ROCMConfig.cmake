665997068427f0bd10de93ad136fcb1448aed4e0
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-10-11 10:37:11 -0600

    Update message type

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-message(NOTICE
+message(STATUS
     "Use of find_package(ROCM) will be deprecated soon, please switch to find_package(ROCmCMakeBuildTools)."
 )
 find_package(ROCmCMakeBuildTools HINTS "${CMAKE_CURRENT_LIST_DIR}")
