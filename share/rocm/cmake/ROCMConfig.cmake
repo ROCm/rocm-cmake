e72e1fe75bcac939ce95f0d5b0f0261853b88773
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2023-11-09 13:55:30 -0700

    Hardcode find path

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -2,4 +2,9 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-find_package(ROCmCMakeBuildTools HINTS "${CMAKE_CURRENT_LIST_DIR}")
+get_filename_component(_new_rocmcmakebuildtools_path "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
+get_filename_component(_new_rocmcmakebuildtools_path "${_new_rocmcmakebuildtools_path}" DIRECTORY)
+set(ROCmCMakeBuildTools_ROOT "${_new_rocmcmakebuildtools_path}/rocmcmakebuildtools/cmake")
+
+# this should find ROCmCMakeBuildTools using the ROCmCMakeBuildTools variable
+find_package(ROCmCMakeBuildTools REQUIRED)
