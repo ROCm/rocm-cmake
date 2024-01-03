de1bb9b65fdb35c6374729205a8284d537f9b9ca
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-01-03 16:05:06 -0700

    fix trailing whitespace

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -7,6 +7,6 @@ get_filename_component(_new_rocmcmakebuildtools_path "${_new_rocmcmakebuildtools
 # two directories up is sufficient for windows search, but linux search requires the share directory
 get_filename_component(_new_rocmcmakebuildtools_path_linux "${_new_rocmcmakebuildtools_path}" DIRECTORY)
 
-find_package(ROCmCMakeBuildTools REQUIRED HINTS 
+find_package(ROCmCMakeBuildTools REQUIRED HINTS
     "${_new_rocmcmakebuildtools_path}"
     "${_new_rocmcmakebuildtools_path_linux}")
