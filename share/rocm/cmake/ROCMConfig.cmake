8ceb225a508d782508bc0e9283e22960f1dfbd6e
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-01-10 13:39:37 -0700

    Clear temporary variables

--- share/rocm/cmake/ROCMConfig.cmake
+++ share/rocm/cmake/ROCMConfig.cmake
@@ -14,3 +14,6 @@ find_dependency(
     HINTS
         "${_new_rocmcmakebuildtools_path}"
         "${_new_rocmcmakebuildtools_path_linux}")
+
+unset(_new_rocmcmakebuildtools_path)
+unset(_new_rocmcmakebuildtools_path_linux)
