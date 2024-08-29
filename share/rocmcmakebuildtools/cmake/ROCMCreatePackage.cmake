d27d809ddbb8ced17d7a07a0b0c7db5d74c9af8f
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-08-29 15:54:20 -0600

    Header-only devel package PROVIDES field fixes (#170)
    * Fix provides field
    
    Previously the code for provides field was not evaluating the CPACK_DEBIAN_PACKAGE_PROVIDES variable, causing CPACK_DEBIAN_PACKAGE_PROVIDES to be included as a literal string provided by the devel package.
    
    * Correct debian/rpm
    
    * Fix misplaced comma
    
    * Remove version from RPM provides field
    
    ---------
    
    Co-authored-by: Eiden Yoshida <47196116+eidenyoshida@users.noreply.github.com>

--- share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
@@ -336,14 +336,29 @@ macro(rocm_create_package)
             )
         endif()
         if(PARSE_HEADER_ONLY OR NOT BUILD_SHARED_LIBS)
-            rocm_join_if_set(", "
-                CPACK_DEBIAN_DEVEL_PACKAGE_PROVIDES
-                CPACK_DEBIAN_PACKAGE_PROVIDES
-                "${CPACK_PACKAGE_NAME} (= ${CPACK_PACKAGE_VERSION})")
-            rocm_join_if_set(", "
-                CPACK_RPM_DEVEL_PACKAGE_PROVIDES
-                CPACK_DEBIAN_PACKAGE_PROVIDES
-                "${CPACK_PACKAGE_NAME} = ${CPACK_PACKAGE_VERSION}")
+            if(DEFINED CPACK_DEBIAN_DEVEL_PACKAGE_PROVIDES)
+                rocm_join_if_set(", "
+                    CPACK_DEBIAN_DEVEL_PACKAGE_PROVIDES
+                    "${CPACK_PACKAGE_NAME} (= ${CPACK_PACKAGE_VERSION})"
+                )
+            else()
+                rocm_join_if_set(", "
+                    CPACK_DEBIAN_DEVEL_PACKAGE_PROVIDES
+                    "${CPACK_DEBIAN_PACKAGE_PROVIDES}"
+                    "${CPACK_PACKAGE_NAME} (= ${CPACK_PACKAGE_VERSION})")
+            endif()
+
+            if(DEFINED CPACK_RPM_DEVEL_PACKAGE_PROVIDES)
+                rocm_join_if_set(", "
+                    CPACK_RPM_DEVEL_PACKAGE_PROVIDES
+                    "${CPACK_PACKAGE_NAME}"
+                )
+            else()
+                rocm_join_if_set(", "
+                    CPACK_RPM_DEVEL_PACKAGE_PROVIDES
+                    "${CPACK_RPM_PACKAGE_PROVIDES}"
+                    "${CPACK_PACKAGE_NAME}")
+            endif()
         else()
             rocm_package_add_dependencies(COMPONENT devel DEPENDS "${CPACK_PACKAGE_NAME} >= ${CPACK_PACKAGE_VERSION}")
         endif()
