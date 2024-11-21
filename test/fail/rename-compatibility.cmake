87c9dc96ce4ae1ca00b5ca430a69e5bdafacdc14
Lauren Wrubleski <Lauren.Wrubleski@amd.com>
2024-11-21 13:21:54 -0700

    Fix tests to ensure find_package(ROCM) emits a deprecation warning

--- test/fail/rename-compatibility.cmake
+++ test/fail/rename-compatibility.cmake
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.
 # ######################################################################################################################
 
-install_dir(${TEST_DIR}/libsimplecompat TARGETS package CMAKE_ARGS -Wno-deprecated)
+install_dir(${TEST_DIR}/libsimplecompat TARGETS package)
 test_check_package(
     NAME simple
     HEADER simple.h
