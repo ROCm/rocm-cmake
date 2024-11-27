4d2bff806b0d1aec5def8b4bcf7e66f6a8481213
Paul <pfultz2@yahoo.com>
2024-11-27 17:30:50 -0600

    Format

--- share/rocmcmakebuildtools/cmake/ROCMCppCheck.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMCppCheck.cmake
@@ -124,10 +124,10 @@ macro(rocm_enable_cppcheck)
         set(CPPCHECK_COMMAND
             ${CPPCHECK_EXE} -q
             # -v --report-progress
-            ${CPPCHECK_FORCE} ${CPPCHECK_EXHAUSTIVE} ${CPPCHECK_INCONCLUSIVE} ${CPPCHECK_BUILD_DIR_FLAG} ${CPPCHECK_PLATFORM_FLAG}
-            ${CPPCHECK_RULE_FILE_ARG} ${CPPCHECK_TEMPLATE_ARG} ${CPPCHECK_ADDONS_ARG} --inline-suppr --error-exitcode=1
-            -j ${CPPCHECK_JOBS} ${CPPCHECK_DEFINES} ${CPPCHECK_UNDEFINES} ${CPPCHECK_INCLUDES}
-            "--relative-paths=${CMAKE_SOURCE_DIR}" --enable=${CPPCHECK_CHECKS}
+            ${CPPCHECK_FORCE} ${CPPCHECK_EXHAUSTIVE} ${CPPCHECK_INCONCLUSIVE} ${CPPCHECK_BUILD_DIR_FLAG}
+            ${CPPCHECK_PLATFORM_FLAG} ${CPPCHECK_RULE_FILE_ARG} ${CPPCHECK_TEMPLATE_ARG} ${CPPCHECK_ADDONS_ARG}
+            --inline-suppr --error-exitcode=1 -j ${CPPCHECK_JOBS} ${CPPCHECK_DEFINES} ${CPPCHECK_UNDEFINES}
+            ${CPPCHECK_INCLUDES} "--relative-paths=${CMAKE_SOURCE_DIR}" --enable=${CPPCHECK_CHECKS}
             --suppressions-list=${CMAKE_BINARY_DIR}/cppcheck-supressions)
 
         add_custom_target(
