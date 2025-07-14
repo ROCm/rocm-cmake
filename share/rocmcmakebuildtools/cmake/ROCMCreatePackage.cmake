b83b0722b35cd8ee7cf59e6bfce99447d44ec40d
ArvindCheru <Aravindan.Cheruvally@amd.com>
2025-07-14 13:01:57 -0400

    Lintian Warning Fix, License Folder Name Update

--- share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
@@ -36,6 +36,7 @@ find_program(DPKG_EXE dpkg)
 
 set(CMAKE_INSTALL_DEFAULT_COMPONENT_NAME runtime)
 set(ROCM_PACKAGE_CREATED FALSE CACHE INTERNAL "Track whether rocm_create_package has been called.")
+set(HDRONLY_LICENSE_INSTALLDIR "" CACHE INTERNAL "License File Install Directory Name")
 
 function(rocm_package_add_rpm_dependencies)
     set(options QUIET)
@@ -402,9 +403,6 @@ macro(rocm_create_package)
 
     file(WRITE ${PROJECT_BINARY_DIR}/debian/postinst "")
     file(WRITE ${PROJECT_BINARY_DIR}/debian/prerm "")
-    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${PROJECT_BINARY_DIR}/debian/postinst;${PROJECT_BINARY_DIR}/debian/prerm")
-    set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/postinst")
-    set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/prerm")
 
     if(PARSE_LDCONFIG AND NOT ${ROCM_DISABLE_LDCONFIG})
         set(LDCONFIG_DIR ${LIB_DIR})
@@ -429,10 +427,24 @@ macro(rocm_create_package)
     if(PARSE_PTH)
         rocm_parse_python_syspath(${LIB_DIR} ${PARSE_NAME})
     endif()
-    rocm_setup_license(${PARSE_HEADER_ONLY})
     if(PARSE_COMPONENTS)
         rocm_set_comp_cpackvar(PARSE_HEADER_ONLY "${PARSE_SUFFIX}" "${PARSE_COMPONENTS}")
     endif()
+    # License Folder to match with Package Name
+    rocm_setup_license(${PARSE_HEADER_ONLY})
+
+    # Lintian Warning Fix: Enable post/pre scripts only if non empty
+    file(SIZE ${PROJECT_BINARY_DIR}/debian/postinst _postinst_file_sz)
+    if(${_postinst_file_sz} GREATER 0)
+      set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${PROJECT_BINARY_DIR}/debian/postinst;${PROJECT_BINARY_DIR}/debian/prerm")
+      set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/postinst")
+    endif()
+    file(SIZE ${PROJECT_BINARY_DIR}/debian/prerm _prerm_file_sz)
+    if(${_prerm_file_sz} GREATER 0)
+      set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/prerm")
+      set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA};${PROJECT_BINARY_DIR}/debian/prerm")
+    endif()
+
     include(CPack)
     set(ROCM_PACKAGE_CREATED TRUE CACHE INTERNAL "Track whether rocm_create_package has been called.")
 endmacro()
@@ -472,7 +484,7 @@ macro(rocm_setup_license HEADER_ONLY)
         elseif((ROCM_USE_DEV_COMPONENT AND ${HEADER_ONLY}) OR NOT BUILD_SHARED_LIBS)
             install(
                 FILES ${CPACK_RESOURCE_FILE_LICENSE}
-                DESTINATION share/doc/${_rocm_cpack_package_name}
+                DESTINATION share/doc/${HDRONLY_LICENSE_INSTALLDIR}
                 COMPONENT devel
             )
         else()
@@ -527,6 +539,15 @@ macro(rocm_compute_component_package_name COMPONENT_NAME BASE_NAME NAME_SUFFIX H
     )
         set(CPACK_DEBIAN_${_component_name_upper}_PACKAGE_NAME
             "${BASE_NAME}${_component_suffix}${_deb_component_partial}")
+    endif()
+
+    # Set install folder for License File
+    if( ROCM_USE_DEV_COMPONENT AND ${HEADER_ONLY} )
+        if( CPACK_RPM_COMPONENT_INSTALL )
+            set( HDRONLY_LICENSE_INSTALLDIR "${BASE_NAME}${_component_suffix}${_rpm_component_partial}")
+        elseif( CPACK_DEB_COMPONENT_INSTALL )
+            set( HDRONLY_LICENSE_INSTALLDIR "${BASE_NAME}${_component_suffix}${_deb_component_partial}")
+        endif()
     endif()
 
     # clean up temporary variables
