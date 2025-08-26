bb18c61579e46741ad8c6d663fedfcd76aa621be
arvindcheru <90783369+arvindcheru@users.noreply.github.com>
2025-08-26 14:43:22 -0400

    Fix Lintian warning for empty pre/post install scripts (#273)
    * Lintian Warning Fix, License Folder Name Update
    
    * Prioritizing Lintian Warning Fixes
    
    * UT FIX Update
    
    * Update comments

--- share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMCreatePackage.cmake
@@ -402,10 +402,6 @@ macro(rocm_create_package)
 
     file(WRITE ${PROJECT_BINARY_DIR}/debian/postinst "")
     file(WRITE ${PROJECT_BINARY_DIR}/debian/prerm "")
-    set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${PROJECT_BINARY_DIR}/debian/postinst;${PROJECT_BINARY_DIR}/debian/prerm")
-    set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/postinst")
-    set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${PROJECT_BINARY_DIR}/debian/prerm")
-
     if(PARSE_LDCONFIG AND NOT ${ROCM_DISABLE_LDCONFIG})
         set(LDCONFIG_DIR ${LIB_DIR})
         if(PARSE_LDCONFIG_DIR)
@@ -417,7 +413,6 @@ macro(rocm_create_package)
             echo \"${LDCONFIG_DIR}\" > /etc/ld.so.conf.d/${PARSE_NAME}.conf
             ldconfig
         ")
-
         file(
             APPEND ${PROJECT_BINARY_DIR}/debian/prerm
             "
@@ -433,8 +428,26 @@ macro(rocm_create_package)
     if(PARSE_COMPONENTS)
         rocm_set_comp_cpackvar(PARSE_HEADER_ONLY "${PARSE_SUFFIX}" "${PARSE_COMPONENTS}")
     endif()
+
+    # Lintian Warning Fix: Enable post/pre scripts only if non empty
+    rocm_check_and_configure_script_files("${PROJECT_BINARY_DIR}/debian/prerm" "${PROJECT_BINARY_DIR}/debian/postinst")
+
     include(CPack)
     set(ROCM_PACKAGE_CREATED TRUE CACHE INTERNAL "Track whether rocm_create_package has been called.")
+endmacro()
+
+# Check script File Size and then configure
+macro(rocm_check_and_configure_script_files PS_SCRIPT_FILE PR_SCRIPT_FILE)
+    file(SIZE ${PS_SCRIPT_FILE} _postinst_file_sz)
+    if(${_postinst_file_sz} GREATER 0)
+        set(CPACK_RPM_POST_INSTALL_SCRIPT_FILE "${PS_SCRIPT_FILE}")
+        set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${PS_SCRIPT_FILE}")
+    endif()
+    file(SIZE ${PR_SCRIPT_FILE} _prerm_file_sz)
+    if(${_prerm_file_sz} GREATER 0)
+        set(CPACK_RPM_PRE_UNINSTALL_SCRIPT_FILE "${PR_SCRIPT_FILE}")
+        set(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA  "${CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA};${PR_SCRIPT_FILE}")
+    endif()
 endmacro()
 
 macro(rocm_setup_license HEADER_ONLY)
