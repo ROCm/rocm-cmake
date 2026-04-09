203720775e622b6c13a864419cdaba67c5718f26
Artur Wojcik <Artur.Wojcik@amd.com>
2026-04-09 20:41:16 +0200

    squashed from add_versioning_win1

--- share/rocmcmakebuildtools/cmake/ROCMSetupVersion.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMSetupVersion.cmake
@@ -1,6 +1,11 @@
 # ######################################################################################################################
-# Copyright (C) 2017 Advanced Micro Devices, Inc.
+# Copyright (C) 2026 Advanced Micro Devices, Inc.
 # ######################################################################################################################
+if(WIN32)
+    cmake_minimum_required(VERSION 3.17 FATAL_ERROR)
+endif()
+
+include_guard(GLOBAL)
 
 macro(rocm_set_parent VAR)
     set(${VAR}
@@ -168,5 +173,74 @@ function(rocm_set_soversion LIBRARY_TARGET SOVERSION)
 
         set_target_properties(${LIBRARY_TARGET} PROPERTIES SOVERSION ${LIB_VERSION_MAJOR})
         set_target_properties(${LIBRARY_TARGET} PROPERTIES VERSION ${LIB_VERSION_STRING})
+    endif()
+endfunction()
+
+function(rocm_add_version_resource TARGET NAME DESCRIPTION)
+    if(WIN32)
+        get_target_property(TARGET_TYPE ${TARGET} TYPE)
+        if (NOT TARGET_TYPE STREQUAL "EXECUTABLE" AND
+            NOT TARGET_TYPE STREQUAL "SHARED_LIBRARY" AND
+            NOT TARGET_TYPE STREQUAL "MODULE_LIBRARY")
+            message(FATAL_ERROR "${CMAKE_CURRENT_FUNCTION}: only EXECUTABLE, SHARED_LIBRARY, "
+                    "and MODULE_LIBRARY target types are supported")
+        endif()
+        get_target_property(FILENAME ${TARGET} OUTPUT_NAME)
+        if(NOT FILENAME)
+            if(TARGET_TYPE STREQUAL "EXECUTABLE")
+                set(FILENAME ${TARGET}${CMAKE_EXECUTABLE_SUFFIX})
+            elseif(TARGET_TYPE STREQUAL "SHARED_LIBRARY")
+                set(FILENAME ${TARGET}${CMAKE_SHARED_LIBRARY_SUFFIX})
+            elseif(TARGET_TYPE STREQUAL "MODULE_LIBRARY")
+                set(FILENAME ${TARGET}${CMAKE_SHARED_MODULE_SUFFIX})
+            endif()
+        endif()
+        string(TIMESTAMP YEAR "%Y")
+        set(RC_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${TARGET}_version.rc")
+        file(WRITE "${RC_OUTPUT}" "#include <winver.h>
+VS_VERSION_INFO VERSIONINFO
+FILEVERSION     ${PROJECT_VERSION_MAJOR},${PROJECT_VERSION_MINOR},${PROJECT_VERSION_PATCH},0
+PRODUCTVERSION  ${PROJECT_VERSION_MAJOR},${PROJECT_VERSION_MINOR},${PROJECT_VERSION_PATCH},0
+FILEFLAGSMASK   VS_FFI_FILEFLAGSMASK
+#ifdef _DEBUG
+FILEFLAGS       VS_FF_DEBUG
+#else
+FILEFLAGS       0
+#endif
+FILEOS          VOS_NT_WINDOWS32
+#ifdef DLL_BUILD
+FILETYPE        VFT_DLL
+#else
+FILETYPE        VFT_APP
+#endif
+FILESUBTYPE     VFT2_UNKNOWN
+BEGIN
+  BLOCK \"StringFileInfo\"
+  BEGIN
+    BLOCK \"040904B0\"
+    BEGIN
+      VALUE \"CompanyName\", \"Advanced Micro Devices, Inc.\\0\"
+      VALUE \"FileDescription\", \"${DESCRIPTION}\\0\"
+      VALUE \"FileVersion\", \"${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}.0\\0\"
+      VALUE \"InternalName\", \"${NAME}\\0\"
+      VALUE \"LegalCopyright\", \"Copyright (c) ${YEAR} Advanced Micro Devices, Inc. All rights reserved.\\0\"
+      VALUE \"OriginalFilename\", \"${FILENAME}\\0\"
+      VALUE \"ProductName\", \"${NAME} ${FILENAME}\\0\"
+      VALUE \"ProductVersion\", \"${PROJECT_VERSION}\\0\"
+    END
+  END
+  BLOCK \"VarFileInfo\"
+  BEGIN
+    VALUE \"Translation\", 0x409, 1200
+  END
+END
+")
+        target_sources(${TARGET} PRIVATE ${RC_OUTPUT})
+        if(TARGET_TYPE STREQUAL "SHARED_LIBRARY" OR TARGET_TYPE STREQUAL "MODULE_LIBRARY")
+            get_source_file_property(RC_OUTPUT_COMPILE_FLAGS ${RC_OUTPUT} COMPILE_FLAGS)
+            set_source_files_properties(${RC_OUTPUT} PROPERTIES
+                    COMPILE_FLAGS "${RC_OUTPUT_COMPILE_FLAGS};-DDLL_BUILD")
+        endif()
+        message(STATUS "Added version resource to ${TARGET}: ${DESCRIPTION}")
     endif()
 endfunction()
