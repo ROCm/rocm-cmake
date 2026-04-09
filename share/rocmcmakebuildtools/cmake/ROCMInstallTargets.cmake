a99c2cae08c3a7bf629d6c4a541f0d6f667040a0
Artur Wojcik <Artur.Wojcik@amd.com>
2026-04-09 16:44:40 +0200

    install PDB files for Debug builds on Windows

--- share/rocmcmakebuildtools/cmake/ROCMInstallTargets.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMInstallTargets.cmake
@@ -233,6 +233,11 @@ function(rocm_install_targets)
                 DESTINATION ${LIB_INSTALL_DIR}
                 COMPONENT ${development}
         )
+        if(WIN32 AND NOT T_TYPE STREQUAL "INTERFACE_LIBRARY")
+            install(FILES $<TARGET_PDB_FILE:${TARGET}>
+                COMPONENT ${runtime}
+                DESTINATION ${BIN_INSTALL_DIR} OPTIONAL)
+        endif()
         rocm_set_install_dir_property(
             TARGETS ${TARGET}
             RUNTIME_DESTINATION ${BIN_INSTALL_DIR}
