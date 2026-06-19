cc8fae010330ebb85b32530463ae1ede7b068a02
Your Name <you@example.com>
2026-06-18 19:24:27 -0500

    Remove pdb generator since its broken on windows

--- share/rocmcmakebuildtools/cmake/ROCMInstallTargets.cmake
+++ share/rocmcmakebuildtools/cmake/ROCMInstallTargets.cmake
@@ -233,13 +233,6 @@ function(rocm_install_targets)
                 DESTINATION ${LIB_INSTALL_DIR}
                 COMPONENT ${development}
         )
-        if(WIN32 AND
-            (T_TYPE STREQUAL "SHARED_LIBRARY" OR
-             T_TYPE STREQUAL "MODULE_LIBRARY" OR
-             T_TYPE STREQUAL "EXECUTABLE"))
-            install(FILES $<TARGET_PDB_FILE:${TARGET}>
-                DESTINATION ${BIN_INSTALL_DIR} OPTIONAL)
-        endif()
         rocm_set_install_dir_property(
             TARGETS ${TARGET}
             RUNTIME_DESTINATION ${BIN_INSTALL_DIR}
